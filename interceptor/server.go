43e2c08e2bcc8f9c2b3b76a2731918b956acdc84
Indra Made <indra.made@frankieone.com>
2024-06-08 13:54:51 +0700

    added basic service

--- interceptor/server.go
+++ interceptor/server.go
@@ -2,19 +2,35 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"math/rand"
+	"time"
 
 	pb "github.com/madeindra/grpc-metadata/interceptor/proto"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
+
+var allowedNonce = map[string]time.Time{}
+
+const nonceDuration = 10 * time.Second
 
 type server struct {
 	pb.UnimplementedSecretServiceServer
 }
 
 func (*server) Nonce(ctx context.Context, _ *emptypb.Empty) (*pb.NonceResponse, error) {
-	return &pb.NonceResponse{}, nil
+	// buat kunci baru
+	genValue := fmt.Sprintf("%d", rand.Int())
+	allowedNonce[genValue] = time.Now().Add(nonceDuration)
+
+	// kirim kunci dalam response
+	return &pb.NonceResponse{
+		Nonce: genValue,
+	}, nil
 }
 
 func (*server) Protected(ctx context.Context, _ *emptypb.Empty) (*pb.ProtectedResponse, error) {
-	return &pb.ProtectedResponse{}, nil
+	return &pb.ProtectedResponse{
+		Message: "Welcome!",
+	}, nil
 }
